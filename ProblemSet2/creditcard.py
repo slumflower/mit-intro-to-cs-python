35bf19f990a43d859b523131141178fe694cb289
P. Johnson <fleurdetaudis@gmail.com>
2020-10-03 20:33:45 -0500

    Update creditcard.py

--- ProblemSet2/creditcard.py
+++ ProblemSet2/creditcard.py
@@ -6,6 +6,12 @@ Created on Sun Feb 24 13:51:59 2019
 @author: portiajohnson
 """
 
+
+#Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
+
+
+
+
 #balance - the outstanding balance on the credit card
 #annualInterestRate - annual interest rate as a decimal
 #monthlyPaymentRate - minimum monthly payment rate as a decimal
