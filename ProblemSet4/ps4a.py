436264644bc8d3e6510f9a5388bf993d6ba62937
P. Johnson <fleurdetaudis@gmail.com>
2020-10-03 20:48:10 -0500

    Update ps4a.py

--- ProblemSet4/ps4a.py
+++ ProblemSet4/ps4a.py
@@ -11,47 +11,7 @@ SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
-# -----------------------------------
-# Helper code
-# (you don't need to understand this helper code)
 
-WORDLIST_FILENAME = "words.txt"
-
-def loadWords():
-    """
-    Returns a list of valid words. Words are strings of lowercase letters.
-    
-    Depending on the size of the word list, this function may
-    take a while to finish.
-    """
-    print("Loading word list from file...")
-    # inFile: file
-    inFile = open(WORDLIST_FILENAME, 'r')
-    # wordList: list of strings
-    wordList = []
-    for line in inFile:
-        wordList.append(line.strip().lower())
-    print("  ", len(wordList), "words loaded.")
-    return wordList
-
-def getFrequencyDict(sequence):
-    """
-    Returns a dictionary where the keys are elements of the sequence
-    and the values are integer counts, for the number of times that
-    an element is repeated in the sequence.
-
-    sequence: string or list
-    return: dictionary
-    """
-    # freqs: dictionary (element_type -> int)
-    freq = {}
-    for x in sequence:
-        freq[x] = freq.get(x,0) + 1
-    return freq
-	
-
-# (end of helper code)
-# -----------------------------------
 
 #
 # Problem #1: Scoring a word
