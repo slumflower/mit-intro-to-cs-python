e9d7d65087dca2c75fb9dc3faa6a54ea9bb5e59a
P. Johnson <fleurdetaudis@gmail.com>
2020-10-03 20:36:18 -0500

    Update pset2.2.py

--- ProblemSet2/pset2.2.py
+++ ProblemSet2/pset2.2.py
@@ -1,5 +1,10 @@
-balance = 3329
-annualInterestRate = 0.2
+#Now write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months. 
+#By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be paid each month.
+
+#In this problem, we will not be dealing with a minimum monthly payment rate.
+
+
+
 
 monthlyInterest = annualInterestRate/12
 fixedPayment = 10
